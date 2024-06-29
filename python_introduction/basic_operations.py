3f17ab9a05b206b4d8d68f542f2a8cebfac0baba
Oby Tonia <anthonian22@gmail.com>
2024-06-29 22:28:13 +0100

    Added Basic operatons

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+#Assign values to variables
+number1 = 10
+number2 = 5
+# Perform Operations
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+# Print results in human-readable format
+print('The Sum of', number1, 'and', number2,'is', sum)
+print('The Difference of', number1, 'and', number2,'is', difference)
+print('The Product of', number1, 'and', number2,'is', product)
