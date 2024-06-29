211d24a8ca63ab002fed185253ac72d450a1c6ac
Oby Tonia <anthonian22@gmail.com>
2024-06-29 22:53:17 +0100

    added simple interest file

---
+++ python_introduction/simple_interests.py
@@ -0,0 +1,8 @@
+# Define variables
+principal = 1000 #Initial investment
+rate = 0.05 #annual interest rate 
+time = 3 #investment time in years
+#Calculate simple interest
+interest = principal * rate * time
+#Print Result
+print ("The simple interest is:",interest)
