1a3cbc824962cc77d10c2c85262d8a7cfcbc465f
Oby Tonia <anthonian22@gmail.com>
2024-06-29 23:39:03 +0100

    added hours_to_seconds file

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+#Define Variable 
+hours = 2
+#Conversion Factor
+second_per_hour = 3600
+#Calculate the Seconds
+seconds = hours * second_per_hour
+#Print Result
+print (f"{hours} hour(s) is {seconds} seconds")
