8180c7ef123272d9df498dccd5e1bcac85ad36c8
Oby Tonia <anthonian22@gmail.com>
2024-07-05 15:22:59 +0100

    created temp. file

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,43 @@
+#Define Global Conversion Factors
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius (fahrenheit):
+    """
+    This function converts a temperature from Fahrenheit to Celsius
+    Args:
+        fahrenheit (float): The temperature in Fahrenheit.
+    Returns:
+        float: The temperature in celsius"""
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+def convert_to_fahrenheit (celsius):
+    """
+    This function converts a temperature from Celsius to Fahrenheit
+    Args:
+        celsius (float): The temperature in Celsius.
+    Returns:
+        float: The temperature in Fahrenheit"""
+    fahrenheit = (celsius + 32) * CELSIUS_TO_FAHRENHEIT_FACTOR
+    return fahrenheit
+def main ():
+    """Prompts the user for temperature and unit, performs conversion, and displays the result."""
+    while True:
+        try:
+           temperature = float (input("Enter a temperature value: "))
+           unit = input ("Enter the unit (C or F): ").upper()
+           if unit == "F":
+              converted_temperature = convert_to_celsius (temperature)
+              converted_unit = "C"
+           elif unit == "C":
+            converted_temperature = convert_to_fahrenheit(temperature)
+            converted_unit = "F"
+           else:
+              raise ValueError("Invalid unit. Please enter 'C' or 'F'.")
+           print(f"{temperature:.2f} degrees {unit} is equal to {converted_temperature:.2f} degrees {converted_unit}.")
+           break  
+        except ValueError as e:
+           print(f"Error: {e}")
+
+#if __name__ == "__main__":
+  #main()
