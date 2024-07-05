e8b31db114d0af46615fddad55b75befa3eef168
Oby Tonia <anthonian22@gmail.com>
2024-07-05 11:11:16 +0100

    added shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,37 @@
+def display_menu ():
+    print("\nShopping List Manager")
+    print("0. Add Item")
+    print("1. Remove Item")
+    print("2. View List")
+    print("3. Exit")
+def main ():
+    shopping_list = []
+    while True:
+        choice = input("Enter your choice:")
+        if choice == '1':
+          item = input("Enter item to add:")
+          shopping_list.append(item)
+          print(f"{item} added to the list.")
+        elif choice == '2':
+           item = input("Enter item to remove: ")
+           if item in shopping_list:
+            shopping_list.remove(item)
+            print(f"{item} removed from the list.")
+           else:
+            print(f"{item} not found in the list.")
+        elif choice == '3':
+           # Display the shopping list
+           if shopping_list:
+            print("\nYour Shopping List:")
+            for item in shopping_list:
+               print(item)
+            else:
+                print("The list is currently empty.")
+        elif choice == '4':
+            print("Exiting Shopping List Manager.")
+            break
+        else:
+              print("Invalid choice. Please try again.")
+    
+if __name__ == "__main__":
+   main()
