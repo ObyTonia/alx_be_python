b517c2a7a9bf47c8439137ac6cf360ff56db13da
Oby Tonia <anthonian22@gmail.com>
2024-07-10 13:49:25 +0100

    modifed test file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,18 +6,18 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
          """Set up the SimpleCalculator instance before each test."""
          self.calc = SimpleCalculator()
-    def test_addition (self):
+    def test_addition(self):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(2, -3), -1)
-    def test_subtraction (self):
+    def test_subtraction(self):
         """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(5, 2),3)
         self.assertEqual(self.calc.subtract(-3, -2),-1)
         self.assertEqual(self.calc.subtract(-3, 2),-5)
-    def test_multiplication (self):
+    def test_multiplication(self):
         """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(5, 2),10)
         self.assertEqual(self.calc.multiply(-3, -2),6)
