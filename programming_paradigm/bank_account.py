4982bf6d856bcb73d7a4705bc03db75c9f70dc21
Oby Tonia <anthonian22@gmail.com>
2024-07-10 11:02:57 +0100

    created bank_account and main file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__ (self, initial_balance=0):
+        self.account_balance = initial_balance
+    def deposit (self, amount):
+       #Deposits the specified amount into the account 
+       self.account_balance += amount
+    def withdraw (self, amount):
+       #Deducts the amount from account balance.Returns True if successful, False if insufficient funds.
+       if amount <= self.account_balance:
+          self.account_balance -= amount
+          return True
+       else:
+          return False
+    def display_balance(self):
+    #Prints the current account balance in a user-friendly format.
+        print(f"Your current balance is: ${self.account_balance:}")
+
